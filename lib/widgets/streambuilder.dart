7b186d14f085b4de3288724b407c3e890423588b
JosipTr <jtr.other13@gmail.com>
2022-06-23 11:00:19 +0200

    Create MyStreamBuilder

---
+++ lib/widgets/streambuilder.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:tutorial/view/home_view.dart';
+import 'package:tutorial/view/login_view.dart';
+import 'package:tutorial/viewmodel.dart/firebaseauth_vievmodel.dart';
+
+class MyStreamBuilder extends StatefulWidget {
+  const MyStreamBuilder({Key? key}) : super(key: key);
+
+  @override
+  State<MyStreamBuilder> createState() => _MyStreamBuilderState();
+}
+
+class _MyStreamBuilderState extends State<MyStreamBuilder> {
+  final _firebaseAuthViewModel = FirebaseAuthViewModel();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _firebaseAuthViewModel.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return HomeView();
+        } else {
+          return LoginView();
+        }
+      }
+    );
+  }
+}
